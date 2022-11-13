9226393a47f685a05e39b846174c837568ffd8b3
carmona44 <dcacarmona44@gmail.com>
2022-11-13 17:41:40 +0100

    contract logic

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,31 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    const metadataURL = "ipfs://QmbbsBVRYxupGTDrLZ9HQuZwMFxLrMJiCEjUSHQGmuJ5sM/";
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+    const buenasChicasContract = await ethers.getContractFactory("BuenasChicas");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const deployedBuenasChicasContract = await buenasChicasContract.deploy(metadataURL);
 
-  await lock.deployed();
+    await deployedBuenasChicasContract.deployed();
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    console.log("BuenasChicas Contract Address:", deployedBuenasChicasContract.address);
+    //0xF41823D6988D4FC31Cc652B1319357F5eA45a61C
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
